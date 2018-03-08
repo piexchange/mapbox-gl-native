7bd8e57ebe7cb8d5a7f9eb0d6148b775a6872946
Minh Nguyễn <mxn@1ec5.org>
2018-03-07 17:47:32 -0800

    [ios, macos] Special-case number conversion without fallbacks

--- platform/darwin/src/NSExpression+MGLAdditions.mm
+++ platform/darwin/src/NSExpression+MGLAdditions.mm
@@ -366,7 +366,11 @@ NSArray *MGLSubexpressionsWithJSONObjects(NSArray *objects) {
             NSExpression *operand = [NSExpression mgl_expressionWithJSONObject:argumentObjects.firstObject];
             argumentObjects = [argumentObjects subarrayWithRange:NSMakeRange(1, argumentObjects.count - 1)];
             NSArray *subexpressions = MGLSubexpressionsWithJSONObjects(argumentObjects);
-            return [NSExpression expressionForFunction:operand selectorName:@"mgl_numberWithFallbackValues:" arguments:subexpressions];
+            if (subexpressions.count == 0) {
+                return [NSExpression expressionForFunction:operand selectorName:@"mgl_number" arguments:@[]];
+            } else {
+                return [NSExpression expressionForFunction:operand selectorName:@"mgl_numberWithFallbackValues:" arguments:subexpressions];
+            }
         } else if ([op isEqualToString:@"to-string"]) {
             NSExpression *operand = [NSExpression mgl_expressionWithJSONObject:argumentObjects.firstObject];
             return [NSExpression expressionForFunction:operand selectorName:@"stringValue" arguments:@[]];
@@ -616,7 +620,8 @@ NSArray *MGLSubexpressionsWithJSONObjects(NSArray *objects) {
                 return [@[@"concat", self.operand.mgl_jsonExpressionObject] arrayByAddingObjectsFromArray:arguments];
             } else if ([function isEqualToString:@"boolValue"]) {
                 return @[@"to-boolean", self.operand.mgl_jsonExpressionObject];
-            } else if ([function isEqualToString:@"mgl_numberWithFallbackValues:"] ||
+            } else if ([function isEqualToString:@"mgl_number"] ||
+                       [function isEqualToString:@"mgl_numberWithFallbackValues:"] ||
                        [function isEqualToString:@"decimalValue"] ||
                        [function isEqualToString:@"floatValue"] ||
                        [function isEqualToString:@"doubleValue"]) {
